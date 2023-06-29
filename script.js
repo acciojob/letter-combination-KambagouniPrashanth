c45b0e5fd09d051d601a07f171208b5c017257e9
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-29 09:45:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-function letterCombinations(input_digit) {
+function letterCombinations(input) {
   //Complete the function
+	let table={
+		"2":"abc",
+		"3":"def",
+		"4":"ghi",
+		"5":"jkl",
+		"6":"mno",
+		"7":"pqrs",
+		"8":"tuv",
+		
+		
+		
+		"9":"wxyz"
+	}
+let solution=[];
+
+	function solve(currindex,calculatedString){
+		if(input.lenght<=currindex){
+			solution.push(calculatedString);
+			return;
+		}
+		let currchar=input[currindex];
+
+		for(let i=0;i<table[currindex].length;i++){
+			solve(currindex+1,calculatedString+table[currchar]);
+			
+		}
+	}
+	
 }
 
 module.exports = letterCombinations;
